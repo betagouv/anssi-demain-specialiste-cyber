1e83527e99024922be2cb34f91fecc1344f0a4ec
Christophe Cadilhac <christophe@malea.dev>
2023-09-11 14:50:36 +0200

    Ajoute du CSS et la police Marianne

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
 app.set("views", "./vues");
+app.use(express.static("public"));
 
 app.get("/", (req, rep) => rep.render("index"));
 
