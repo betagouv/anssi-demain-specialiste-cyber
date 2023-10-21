435cae1f4877e4715594060a33e36634320fff23
Christophe Cadilhac <christophe@malea.dev>
2023-10-21 23:32:54 +0200

    Ajoute toutes les ressources d'enseignant

--- referentiel.js
+++ referentiel.js
@@ -24,7 +24,69 @@ const donnees = {
       idVideo: "analyste-incidents",
     },
   ],
+  ressourcesEnseignant: [
+    {
+      titre: "Formez vos élèves à la cyber",
+      accroche:
+        "Vous pouvez vous-même formez vos élèves à la cybersécurité grâce au kit pédagogique CyberEnjeux et à la présentation.",
+      ressources: [
+        {
+          nom: "Télécharger 13 fiches et cartes pédagogiques sur la cybersécurité et une présentation clé en main",
+          fichier: "PPTX – 61,88 Ko",
+          url: "#",
+          classe: "telechargement",
+        },
+        {
+          nom: "Organiser un hackaton CyberEnjeux dans votre établissement pour former vos élèves par la création de jeux",
+          fichier: "PDF – 61,88 Ko",
+          url: "#",
+          classe: "telechargement",
+        },
+        {
+          nom: "Découvrez plus d’informations sur Eduscol",
+          details:
+            "Eduscol la page éducation et cybersécurité du Ministère de l’Éducation Nationale et de la Jeunesse",
+          fichier: "Accéder à Eduscol",
+          url: "#",
+          classe: "site",
+        },
+      ],
+    },
+    {
+      titre: "Parlez des métiers cyber",
+      accroche: "Des ressources pour pouvoir parler de la cyber à vos élèves :",
+      ressources: [
+        {
+          nom: "Panorama des métiers de la cybersécurité",
+          fichier: "PDF – 61,88 Ko",
+          url: "#",
+          classe: "telechargement",
+        },
+        {
+          nom: "Référentiel des compétences des métiers de la cybersécurité (PDF)",
+          fichier: "PDF – 61,88 Ko",
+          url: "#",
+          classe: "telechargement",
+        },
+      ],
+    },
+    {
+      titre: "Guidez les élèves vers des formations labélisées",
+      ressources: [
+        {
+          nom: "Découvrir les formations",
+          details:
+            "Sur son site l’ANSSI répertorie toutes les formations cyber labelisées, n’hésitez pas à en prendre connaissances pour en parler à vos élèves.",
+          fichier: "Accéder aux formations cyber labelisées ",
+          url: "#",
+          classe: "site",
+        },
+      ],
+    },
+  ],
 };
+
+const lesRessourcesEnseignant = () => donnees.ressourcesEnseignant;
 
 const videoAgirAleatoire = () => {
   const indexVideo = Math.floor(Math.random() * donnees.videosAgir.length);
@@ -45,4 +107,9 @@ const metiersAuHasard = (combien) => {
   return selection;
 };
 
-module.exports = { metiersAuHasard, tousLesMetiers, videoAgirAleatoire };
+module.exports = {
+  lesRessourcesEnseignant,
+  metiersAuHasard,
+  tousLesMetiers,
+  videoAgirAleatoire,
+};
