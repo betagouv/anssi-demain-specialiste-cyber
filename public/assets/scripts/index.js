d22eff4b1690241f9f3ab62f97febfa879330138
Christophe Cadilhac <christophe@malea.dev>
2023-10-23 14:52:50 +0200

    Branche flèches gauche et droite du carrousel Stats

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -1,14 +1,21 @@
 $(document).ready(() => {
-  const $carrouselMetier = $("#carrousel-fiches-metiers");
-
-  const glisseVers = (sens) => {
+  const glisseMetiersVers = (sens) => {
+    const $carrouselMetier = $("#carrousel-fiches-metiers");
     const uneImage = $carrouselMetier.find("img").width();
     const decalageCourant = $carrouselMetier.scrollLeft();
     const troisQuart = uneImage * (3 / 4);
     $carrouselMetier.scrollLeft(decalageCourant + troisQuart * sens);
   };
+  $(".fleche-droite", "#metiers").on("click", () => glisseMetiersVers(+1));
+  $(".fleche-gauche", "#metiers").on("click", () => glisseMetiersVers(-1));
 
-  const $navigation = $("#metiers .carrousel-navigation ");
-  $(".fleche-droite", $navigation).on("click", () => glisseVers(+1));
-  $(".fleche-gauche", $navigation).on("click", () => glisseVers(-1));
+  const glisseStatsVers = (sens) => {
+    const $carrouselStats = $("#infos");
+    const uneImage = $carrouselStats.find(".info").width();
+    const decalageCourant = $carrouselStats.scrollLeft();
+    const troisQuart = uneImage * (3 / 4);
+    $carrouselStats.scrollLeft(decalageCourant + troisQuart * sens);
+  };
+  $(".fleche-droite", "#stats").on("click", () => glisseStatsVers(+1));
+  $(".fleche-gauche", "#stats").on("click", () => glisseStatsVers(-1));
 });
