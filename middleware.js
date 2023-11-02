ad5d9839906e7b5e6ade48ba884fba2fc0a72464
ThibaudMZN <thibaud.marazano@gmail.com>
2023-11-02 10:13:22 +0100

    Met en place un `rate-limit`

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+const rateLimit = require("express-rate-limit");
 const { environnement } = require("./environnement");
 
 const patienteJusqueMep = (requete, reponse, suite) => {
@@ -12,4 +13,20 @@ const patienteJusqueMep = (requete, reponse, suite) => {
   suite();
 };
 
-module.exports = { patienteJusqueMep };
+const protectionLimiteTrafic = () => {
+  const uneMinute = 60 * 1000;
+  const maxParFenetreParIp = 600;
+
+  return rateLimit({
+    windowMs: uneMinute,
+    max: maxParFenetreParIp,
+    handler: (requete, reponse) => {
+      reponse.render("erreurTropDeTrafic");
+    },
+    keyGenerator: (requete) =>
+      // Utilise l'IP de l'utilisateur : https://doc.scalingo.com/platform/internals/routing
+      requete.headers["x-real-ip"],
+  });
+};
+
+module.exports = { patienteJusqueMep, protectionLimiteTrafic };
