a1f2d58cbd9d06ebae31d9ff5fff4b62b62b8188
ThibaudMZN <thibaud.marazano@gmail.com>
2023-11-03 16:19:58 +0100

    Active Matomo en présence de la variable d'environnement MATOMO_SITE_ID

---
+++ app/public/assets/scripts/matomo.js
@@ -0,0 +1,16 @@
+(() => {
+    window._paq = window._paq || [];
+    const { _paq } = window;
+
+    _paq.push(["disableCookies"]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+
+    const u = "https://stats.beta.gouv.fr/";
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', document.getElementById("script-matomo").getAttribute("data-site-id")]);
+    const d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true;
+    g.src = u + 'matomo.js';
+    s.parentNode.insertBefore(g, s);
+})();
