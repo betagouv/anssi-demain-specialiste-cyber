efd26cb73689a5586590df62ef09a6681bd98b63
ThibaudMZN <thibaud.marazano@gmail.com>
2023-11-08 17:18:07 +0100

    Ajoute l'opt-out pour Matomo

--- app/public/assets/scripts/matomo.js
+++ app/public/assets/scripts/matomo.js
@@ -1,16 +1,23 @@
 (() => {
+  if (localStorage.getItem("optOutMatomo") !== "true") {
     window._paq = window._paq || [];
     const { _paq } = window;
 
     _paq.push(["disableCookies"]);
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
+    _paq.push(["trackPageView"]);
+    _paq.push(["enableLinkTracking"]);
 
     const u = "https://stats.beta.gouv.fr/";
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', document.getElementById("script-matomo").getAttribute("data-site-id")]);
-    const d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    _paq.push(["setTrackerUrl", u + "matomo.php"]);
+    _paq.push([
+      "setSiteId",
+      document.getElementById("script-matomo").getAttribute("data-site-id"),
+    ]);
+    const d = document,
+      g = d.createElement("script"),
+      s = d.getElementsByTagName("script")[0];
     g.async = true;
-    g.src = u + 'matomo.js';
+    g.src = u + "matomo.js";
     s.parentNode.insertBefore(g, s);
+  }
 })();
