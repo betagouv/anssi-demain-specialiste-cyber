026cd673b5cb1f2cdc331c16eb7ca77545cfdebe
ThibaudMZN <thibaud.marazano@gmail.com>
2023-11-08 17:18:07 +0100

    Ajoute l'opt-out pour Matomo

---
+++ app/public/assets/scripts/optout-matomo.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+  const modifieLabelConsentement = () => {
+    const optOutMatomo = localStorage.getItem("optOutMatomo") === "true";
+    $("#label-consentement-matomo").text(
+      optOutMatomo
+        ? "Vous n'êtes actuellement pas suivi(e). Cochez cette case pour ne plus être exclu(e)."
+        : "Vous êtes suivi(e) de manière anonyme. Décochez cette case pour vous exclure du suivi.",
+    );
+  };
+
+  $("#consentement-matomo").on("change", () => {
+    const optOutMatomo = !$("#consentement-matomo").is(":checked");
+    localStorage.setItem("optOutMatomo", optOutMatomo);
+    modifieLabelConsentement();
+  });
+
+  modifieLabelConsentement();
+});
