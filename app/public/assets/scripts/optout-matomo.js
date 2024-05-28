8410d4964f70df39565a2dbdf4bff5f4dc98832a
Christophe Cadilhac <christophe@malea.dev>
2024-05-28 15:31:36 +0200

    [CORRECTION] Corrige le coché/décoché de la checkbox Matomo
    Avec l'implémentation précédente, on avait un état incohérent au refresh de la page.
    La case était toujours cochée. Le texte, lui, était correct.

--- app/public/assets/scripts/optout-matomo.js
+++ app/public/assets/scripts/optout-matomo.js
@@ -1,18 +1,18 @@
-$(document).ready(() => {
-  const modifieLabelConsentement = () => {
-    const optOutMatomo = localStorage.getItem("optOutMatomo") === "true";
-    $("#label-consentement-matomo").text(
-      optOutMatomo
-        ? "Vous n'êtes actuellement pas suivi(e). Cochez cette case pour ne plus être exclu(e)."
-        : "Vous êtes suivi(e) de manière anonyme. Décochez cette case pour vous exclure du suivi.",
-    );
-  };
+const modifieCheckboxConsentement = (estOptOut) => {
+  $("#consentement-matomo").prop("checked", !estOptOut);
+  $("#label-consentement-matomo").text(
+    estOptOut
+      ? "Vous n'êtes actuellement pas suivi(e). Cochez cette case pour ne plus être exclu(e)."
+      : "Vous êtes suivi(e) de manière anonyme. Décochez cette case pour vous exclure du suivi.",
+  );
+};
 
+$(document).ready(() => {
   $("#consentement-matomo").on("change", () => {
     const optOutMatomo = !$("#consentement-matomo").is(":checked");
-    localStorage.setItem("optOutMatomo", optOutMatomo);
-    modifieLabelConsentement();
+    localStorage.setItem("optOutMatomo", String(optOutMatomo));
+    modifieCheckboxConsentement(optOutMatomo);
   });
 
-  modifieLabelConsentement();
+  modifieCheckboxConsentement(localStorage.getItem("optOutMatomo") === "true");
 });
