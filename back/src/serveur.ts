cc0c26679c29a84024b6e439fee391036f1f9fb4
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-08-27 16:05:05 +0200

    Rend la page Catalogue depuis le back

--- back/src/serveur.ts
+++ back/src/serveur.ts
@@ -1,18 +1,14 @@
-import express from "express"
+import { creeServeur } from "./api/dsc";
 
-const app = express()
-
-app.get("/", (requete, reponse) => {
-    reponse.send("Bonjour DSC")
-})
+const app = creeServeur();
 
 const port = process.env.PORT || 3000;
 
 const serveur = app.listen(port, () => {
-    console.log(`Le serveur écoute sur le port ${port}`)
-})
+  console.log(`Le serveur écoute sur le port ${port}`);
+});
 
 serveur.on("error", (erreur) => {
-    console.error(erreur)
-    process.exit(1)
-})
+  console.error(erreur);
+  process.exit(1);
+});
---
+++ back/tests/ressourceCatalogue.spec.ts
@@ -0,0 +1,33 @@
+import { Express } from "express";
+import request from "supertest";
+import { describe, expect, it } from "vitest";
+import { creeServeur } from "../src/api/dsc";
+
+describe("La page Catalogue", () => {
+  const appelleCatalogue = (serveur: Express) =>
+    request(serveur).get("/catalogue");
+
+  it("répond 200", async () => {
+    const serveur = creeServeur();
+
+    const reponse = await appelleCatalogue(serveur);
+
+    expect(reponse.status).toEqual(200);
+  });
+
+  it("répond avec un contenu HTML", async () => {
+    const serveur = creeServeur();
+
+    const reponse = await appelleCatalogue(serveur);
+
+    expect(reponse.headers["content-type"]).toEqual("text/html; charset=utf-8");
+  });
+
+  it("répond avec le contenu HTML du catalogue", async () => {
+    const serveur = creeServeur();
+
+    const reponse = await appelleCatalogue(serveur);
+
+    expect(reponse.text).toMatchSnapshot();
+  });
+});
