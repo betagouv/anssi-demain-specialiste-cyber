525d81b86d7342c4a0440d584ad2376812fd3ab4
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-08-27 15:14:27 +0200

    Réinitialise le site
    ...pour la nouvelle version

--- media/environnement.js
+++
@@ -1,6 +0,0 @@
-const environnement = () => ({
-  ipAutorisees: () => process.env.ADRESSES_IP_AUTORISEES?.split(",") ?? [],
-  activerFiltrageIp: () => environnement().ipAutorisees().length > 0
-});
-
-module.exports = { environnement };
