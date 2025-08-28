acbdecd6c1745a5a79ab66c566a1dbd192eaa81b
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-08-28 11:16:22 +0200

    Limite le nombre le requêtes par minutes
    ...en utilisant la lib de l’ANSSI

---
+++ back/tests/api/dsc.spec.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it } from 'vitest';
+import { creeServeur } from '../../src/api/dsc';
+import request from 'supertest';
+
+describe("L'API DSC", () => {
+  describe('concernant la limitation de trafic', () => {
+    const creeServeurAvecLimiteDeRequetes = (
+      maxRequetesParMinute: number
+    ) =>
+      creeServeur({
+        serveurLab: {
+          reseau: {
+            trustProxy: 0,
+            ipAutorisees: false,
+            maxRequetesParMinute,
+          },
+        },
+      });
+
+    it('respecte une limite', async () => {
+      const serveur = creeServeurAvecLimiteDeRequetes(1);
+      const requete = request(serveur);
+      await requete.get('/');
+
+      const reponse = await requete.get('/');
+
+      expect(reponse.status).toEqual(429);
+    });
+
+    it('utilise la configuration', async () => {
+      const serveur = creeServeurAvecLimiteDeRequetes(2);
+      const requete = request(serveur);
+      await requete.get('/');
+
+      const reponse = await requete.get('/');
+
+      expect(reponse.status).toEqual(200);
+    });
+  });
+});
