8f1b26f39efa2d7ec02d086c10493b7361329884
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-08-28 09:19:09 +0200

    Corrige les erreurs de lint

--- back/src/api/dsc.ts
+++ back/src/api/dsc.ts
@@ -3,11 +3,11 @@ import express from "express";
 export const creeServeur = () => {
   const app = express();
 
-  app.get("/", (requete, reponse) => {
+  app.get("/", (_requete, reponse) => {
     reponse.send("Bonjour DSC");
   });
 
-  app.get("/catalogue", (requete, reponse) => {
+  app.get("/catalogue", (_requete, reponse) => {
     reponse.sendFile(`./pages/catalogue.html`, {
       root: ".",
     });
