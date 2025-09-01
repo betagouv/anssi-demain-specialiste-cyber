e86630c7270d53f0b0e592618c53b6dc910fa8e7
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-01 17:59:38 +0200

    Chiffre et dechiffre les données utilisateur

--- back/src/infra/entrepotUtilisateurPostgres.ts
+++ back/src/infra/entrepotUtilisateurPostgres.ts
@@ -2,11 +2,16 @@ import Knex from 'knex';
 import config from '../../knexfile';
 import { EntrepotUtilisateur } from '../metier/entrepotUtilisateur';
 import { Utilisateur } from '../metier/utilisateur';
-import { AdaptateurChiffrement } from './adaptateurChiffrement';
+import { AdaptateurChiffrement, ObjetChiffre } from './adaptateurChiffrement';
 import { AdaptateurHachage } from './adaptateurHachage';
+
+type DonneesUtilisateurEnClair = {
+  email: string;
+};
 
 type UtilisateurBDD = {
   email_hache: string;
+  donnees: ObjetChiffre;
 };
 
 export class EntrepotUtilisateurPostgres implements EntrepotUtilisateur {
@@ -26,11 +31,12 @@ export class EntrepotUtilisateurPostgres implements EntrepotUtilisateur {
     this.adaptateurChiffrement = adaptateurChiffrement;
   }
 
-  async ajoute(_utilisateur: Utilisateur) {
-    {
-      throw new Error();
-    }
+  async ajoute(utilisateur: Utilisateur) {
+    await this.knex('utilisateurs').insert(
+      this.utilisateurBDDAvecDonneesChiffrees(utilisateur)
+    );
   }
+
   async parEmailHache(emailHache: string): Promise<Utilisateur | undefined> {
     if (!emailHache) return undefined;
     const utilisateur = await this.knex('utilisateurs')
@@ -54,14 +60,35 @@ export class EntrepotUtilisateurPostgres implements EntrepotUtilisateur {
     throw new Error();
   }
 
-  private hydrateUtilisateur(_utilisateur: UtilisateurBDD): Utilisateur {
+  private hydrateUtilisateur(utilisateur: UtilisateurBDD): Utilisateur {
+    const donnees = this.dechiffreDonneesUtilisateur(utilisateur);
     return new Utilisateur({
       cguAcceptees: true,
-      email: '',
+      email: donnees.email,
       infolettreAcceptee: true,
       nom: '',
       prenom: '',
       siretEntite: '',
     });
+  }
+
+  private utilisateurBDDAvecDonneesChiffrees(
+    utilisateur: Utilisateur
+  ): UtilisateurBDD {
+    const donneesEnClair: DonneesUtilisateurEnClair = {
+      email: utilisateur.email,
+    };
+    const donneesChiffrees = this.adaptateurChiffrement.chiffre(donneesEnClair);
+    return {
+      donnees: donneesChiffrees,
+      email_hache: this.adaptateurHachage.hache(utilisateur.email),
+    };
+  }
+
+  private dechiffreDonneesUtilisateur(
+    utilisateur: UtilisateurBDD
+  ): DonneesUtilisateurEnClair {
+    const { donnees } = utilisateur;
+    return this.adaptateurChiffrement.dechiffre(donnees);
   }
 }
