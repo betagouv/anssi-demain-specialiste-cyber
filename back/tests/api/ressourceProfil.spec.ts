736f98c7ee75734feba0630fc261d86174f38c1e
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-02 16:08:34 +0200

    Renvoie l'email de l'utilisateur
    ...lorsqu'il récupère son profil

--- back/tests/api/ressourceProfil.spec.ts
+++ back/tests/api/ressourceProfil.spec.ts
@@ -5,6 +5,7 @@ import { creeServeur } from '../../src/api/dsc';
 import { EntrepotUtilisateur } from '../../src/metier/entrepotUtilisateur';
 import { Utilisateur } from '../../src/metier/utilisateur';
 import { EntrepotUtilisateurMemoire } from '../infra/entrepotUtilisateurMemoire';
+import { encodeSession } from './cookie';
 import { configurationDeTestDuServeur } from './fauxObjets';
 
 describe('La ressource Profil', () => {
@@ -12,7 +13,7 @@ describe('La ressource Profil', () => {
   let entrepotUtilisateur: EntrepotUtilisateur;
 
   const jeanneDupont = new Utilisateur({
-    email: 'jeanne.dupont',
+    email: 'jeanne.dupont@mail.com',
     cguAcceptees: true,
     infolettreAcceptee: true,
     prenom: '',
@@ -34,6 +35,18 @@ describe('La ressource Profil', () => {
       const reponse = await request(serveur).get('/api/profil');
 
       expect(reponse.status).toEqual(200);
+    });
+
+    it("renvoie les informations de l'utilisateur", async () => {
+      const cookie = encodeSession({
+        email: 'jeanne.dupont@mail.com',
+      });
+
+      const reponse = await request(serveur)
+        .get('/api/profil')
+        .set('Cookie', [cookie]);
+
+      expect(reponse.body.email).toEqual('jeanne.dupont@mail.com');
     });
   });
 });
