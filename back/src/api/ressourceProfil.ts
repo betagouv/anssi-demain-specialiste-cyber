bc5e844be48a84c9c369b31d97f01c63a420b027
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-02 16:12:15 +0200

    Invalide la session
    ... lors de la demande de profil, si le jeton est invalide

--- back/src/api/ressourceProfil.ts
+++ back/src/api/ressourceProfil.ts
@@ -1,12 +1,19 @@
 import { Request, Response, Router } from 'express';
+import { ConfigurationServeur } from './dsc';
 
-export const ressourceProfil = () => {
+export const ressourceProfil = ({ adaptateurJWT }: ConfigurationServeur) => {
   const routeur = Router();
 
   routeur.get('/', async (requete: Request, reponse: Response) => {
-    reponse.send({
-      email: requete.session?.email,
-    });
+    try {
+      adaptateurJWT.decode(requete.session?.token);
+    } catch {
+      reponse.clearCookie('session');
+    } finally {
+      reponse.send({
+        email: requete.session?.email,
+      });
+    }
   });
 
   return routeur;
