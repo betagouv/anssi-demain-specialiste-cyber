882bc10f9c44bb7ba73d3bfa739c22e4403aa8f2
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-09-02 11:24:59 +0200

    Ajoute un service de vérification des secrets de hachage

---
+++ back/src/infra/entrepotSecretHachagePostgres.ts
@@ -0,0 +1,21 @@
+import Knex from 'knex';
+import config from '../../knexfile';
+
+type SecretHachage = { version: number; empreinte: string };
+
+export interface EntrepotSecretHachage {
+  tous: () => Promise<SecretHachage[]>;
+}
+
+export class EntrepotSecretHachagePostgres implements EntrepotSecretHachage {
+  knex: Knex.Knex;
+
+  constructor() {
+    this.knex = Knex(config);
+  }
+
+  async tous(): Promise<SecretHachage[]> {
+    const empreintes = await this.knex('secrets_hachage');
+    return empreintes.map(({version, empreinte}) => ({version, empreinte}));
+  }
+}
