19fa3428dbd24b9ada8aa0418105ff7d038aefbc
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-09-02 10:45:58 +0200

    [CATALOGUE] Récupère et affiche les sélections

--- front/src/ressourceCyber.ts
+++ front/src/ressourceCyber.ts
@@ -2,6 +2,7 @@ export type RessourceCyber = {
   id: number;
   titre: string;
   thematiques: string[];
+  selections: string[];
 };
 
 export const lesRessourcesCyberTriees = (data: RessourceCyber[]) => {
@@ -12,4 +13,12 @@ export const lesThematiquesCyber = (ressourcesCyber: RessourceCyber[]) => {
   return Array.from(
     new Set(ressourcesCyber.flatMap((r) => r.thematiques))
   ).sort();
+};
+
+export const lesSelectionsDesRessourcesCyber = (
+  ressourcesCyber: RessourceCyber[]
+): string[] => {
+  return Array.from(new Set(ressourcesCyber.flatMap((r) => r.selections))).sort(
+    (s1, s2) => s1.localeCompare(s2)
+  );
 };
--- front/tests/ressourceCyber.spec.ts
+++ front/tests/ressourceCyber.spec.ts
@@ -1,14 +1,70 @@
 import { describe, expect, it } from 'vitest';
-import { lesThematiquesCyber } from '../src/ressourceCyber';
+import {
+  lesSelectionsDesRessourcesCyber,
+  lesThematiquesCyber,
+  RessourceCyber,
+} from '../src/ressourceCyber';
 
 describe('Les ressources Cyber', () => {
   it('sort la liste des thématiques', () => {
     const thematiques = lesThematiquesCyber([
-      { id: 1, titre: 'Ressource 1', thematiques: ['Réseau', 'Sécurité'] },
-      { id: 2, titre: 'Ressource 2', thematiques: ['Cryptographie'] },
-      { id: 3, titre: 'Ressource 3', thematiques: ['Réseau', 'Cryptographie'] },
+      new ConstructeurRessourceCyber()
+        .avecThematiques(['Réseau', 'Sécurité'])
+        .construis(),
+      new ConstructeurRessourceCyber()
+        .avecThematiques(['Cryptographie'])
+        .construis(),
+      new ConstructeurRessourceCyber()
+        .avecThematiques(['Réseau', 'Cryptographie'])
+        .construis(),
     ]);
 
     expect(thematiques).toStrictEqual(['Cryptographie', 'Réseau', 'Sécurité']);
   });
+
+  it('sort la liste des sélections', () => {
+    const selections = lesSelectionsDesRessourcesCyber([
+      new ConstructeurRessourceCyber()
+        .avecSelections(['Parents', 'Enseignants'])
+        .construis(),
+      new ConstructeurRessourceCyber()
+        .avecSelections(['Enseignants', 'Élèves'])
+        .construis(),
+      new ConstructeurRessourceCyber()
+        .avecSelections(['Parents', 'Élèves'])
+        .construis(),
+    ]);
+
+    expect(selections).toStrictEqual(['Élèves', 'Enseignants', 'Parents']);
+  });
 });
+
+interface Constructeur<T> {
+  construis(): T;
+}
+
+class ConstructeurRessourceCyber implements Constructeur<RessourceCyber> {
+  private _selections: string[] = [];
+  private _id: number = 1000;
+  private _titre: string = 'Un titre';
+  private _thematiques: string[] = [];
+
+  avecSelections(selections: string[]): ConstructeurRessourceCyber {
+    this._selections = selections;
+    return this;
+  }
+
+  avecThematiques(thematiques: string[]): ConstructeurRessourceCyber {
+    this._thematiques = thematiques;
+    return this;
+  }
+
+  construis(): RessourceCyber {
+    return {
+      id: this._id,
+      titre: this._titre,
+      thematiques: this._thematiques,
+      selections: this._selections,
+    };
+  }
+}
