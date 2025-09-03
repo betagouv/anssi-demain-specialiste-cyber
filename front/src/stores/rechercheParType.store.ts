f0e9c4909cb54e6b425de919ecec44c3e36f21c5
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-09-03 11:03:52 +0200

    [CATALOGUE] Effectue le filtre sur le type

---
+++ front/src/stores/rechercheParType.store.ts
@@ -0,0 +1,14 @@
+import { get, writable } from 'svelte/store';
+import type { RessourceCyber } from '../ressourceCyber';
+
+const types = writable<string[]>([]);
+
+export const rechercheParType = {
+  subscribe: types.subscribe,
+  set: types.set,
+  reinitialise: () => types.set([]),
+  ok: (ressourceCyber: RessourceCyber) =>
+    ressourceCyber.types.some(
+      (type) => !get(types).length || get(types).includes(type)
+    ),
+};
--- front/src/stores/ressourcesCyberFiltrees.store.ts
+++ front/src/stores/ressourcesCyberFiltrees.store.ts
@@ -3,18 +3,21 @@ import {
   lesNiveauxDesRessourcesCyber,
   lesSelectionsDesRessourcesCyber,
   lesThematiquesCyber,
+  lesTypesDesRessourcesCyber,
   type RessourceCyber,
 } from '../ressourceCyber';
 import { rechercheParNiveau } from './rechercheParNiveau.store';
 import { rechercheParSelection } from './rechercheParSelection.store';
 import { rechercheParThematique } from './rechercheParThematique.store';
 import { ressourcesCyberStore } from './ressourcesCyber.store';
+import { rechercheParType } from './rechercheParType.store';
 
 export type RessourcesCyberFiltrees = {
   resultat: RessourceCyber[];
   thematiques: string[];
   selections: string[];
   niveaux: string[];
+  types: string[];
 };
 
 export const ressourcesCyberFiltrees = derived(
@@ -23,19 +26,22 @@ export const ressourcesCyberFiltrees = derived(
     rechercheParThematique,
     rechercheParSelection,
     rechercheParNiveau,
+    rechercheParType,
   ],
   ([$store]): RessourcesCyberFiltrees => {
     const resultat = $store.filter(
       (ressourceCyber: RessourceCyber) =>
         rechercheParThematique.ok(ressourceCyber) &&
         rechercheParSelection.ok(ressourceCyber) &&
-        rechercheParNiveau.ok(ressourceCyber)
+        rechercheParNiveau.ok(ressourceCyber) &&
+        rechercheParType.ok(ressourceCyber)
     );
 
     const thematiques = lesThematiquesCyber($store);
     const selections = lesSelectionsDesRessourcesCyber($store);
     const niveaux = lesNiveauxDesRessourcesCyber($store);
+    const types = lesTypesDesRessourcesCyber($store);
 
-    return { resultat, thematiques, selections, niveaux };
+    return { resultat, thematiques, selections, niveaux, types };
   }
 );
