7403a357d9e594b0c8e21c6401078bdc62d01487
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-09-04 12:32:24 +0200

    [CATALOGUE] Affiche la zone de recherche
    ... et l'applique sur le catalogue

---
+++ front/src/RechercheTextuelle.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  type Props = {
+    recherche: string;
+    miseEnAvant?: string;
+  };
+  let { recherche = $bindable(), miseEnAvant = 'Rechercher' }: Props = $props();
+</script>
+
+<fieldset>
+  <input type="text" bind:value={recherche} placeholder={miseEnAvant} />
+</fieldset>
+
+<style lang="scss">
+  input {
+    width: 100%;
+  }
+</style>
