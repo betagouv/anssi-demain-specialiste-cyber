826abd991dac2b001cf08eee7c7703c20d6cf3fe
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-09-09 17:59:27 +0200

    Ajoute de hash des fichiers générés par Vite
    ... en production uniquement (pas en dev car ça complique le HMR)

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,8 +1,9 @@
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import * as Vite from 'vite';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }: Vite.UserConfig) => ({
   plugins: [
     svelte({
       compilerOptions: { customElement: true },
@@ -10,17 +11,23 @@ export default defineConfig({
     }),
   ],
   build: {
+    manifest: true,
     cssCodeSplit: false,
     rollupOptions: {
       input: {
         // exporte tous les WebComponents dans le script dsc.js
         dsc: 'src/index.ts',
       },
-      output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-      },
+      output:
+        mode === 'development'
+          ? {
+              entryFileNames: 'assets/[name].js',
+              assetFileNames: 'assets/[name].[ext]',
+            }
+          : {
+              entryFileNames: 'assets/[name].[hash].js',
+              assetFileNames: 'assets/[name].[hash].[ext]',
+            },
     },
     emptyOutDir: true,
     sourcemap: true,
@@ -28,4 +35,4 @@ export default defineConfig({
   define: {
     'process.env.NODE_ENV': "'production'",
   },
-});
+}));
