bf3b67d676a2277521636a8a598aa3c0aad206ed
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-09-11 11:24:52 +0200

    [DEPOSE-JEU] Valide le nom du jeu
    ...qui doit être fourni et non vide

--- back/src/api/ressourceJeux.ts
+++ back/src/api/ressourceJeux.ts
@@ -1,6 +1,13 @@
-import { Request, Response, Router } from 'express';
+import {
+  NextFunction,
+  Request,
+  RequestHandler,
+  Response,
+  Router,
+} from 'express';
 import { Jeu } from '../metier/jeu';
 import { ConfigurationServeur } from './dsc';
+import z from 'zod';
 
 export const ressourceJeux = ({
   adaptateurJWT,
@@ -8,15 +15,38 @@ export const ressourceJeux = ({
 }: ConfigurationServeur) => {
   const routeur = Router();
 
-  routeur.post('/', async (requete: Request, reponse: Response) => {
-    try {
-      adaptateurJWT.decode(requete.session?.token);
-      const { nom } = requete.body;
-      entrepotJeux.ajoute(new Jeu({ nom }));
-      reponse.sendStatus(201);
-    } catch {
-      reponse.sendStatus(401);
-    }
+  const schema = z.strictObject({
+    nom: z
+      .string('Le nom est obligatoire')
+      .trim()
+      .min(1, 'Le nom est obligatoire'),
   });
+
+  const valideLaCoherenceDuCorps = (objet: z.ZodType): RequestHandler => {
+    return async (requete: Request, reponse: Response, suite: NextFunction) => {
+      const resultat = objet.safeParse(requete.body);
+      if (!resultat.success) {
+        return reponse
+          .status(400)
+          .json({ erreur: resultat.error.issues[0].message });
+      }
+      return suite();
+    };
+  };
+
+  routeur.post(
+    '/',
+    valideLaCoherenceDuCorps(schema),
+    async (requete: Request, reponse: Response) => {
+      try {
+        adaptateurJWT.decode(requete.session?.token);
+        const { nom } = requete.body;
+        entrepotJeux.ajoute(new Jeu({ nom }));
+        reponse.sendStatus(201);
+      } catch {
+        reponse.sendStatus(401);
+      }
+    },
+  );
   return routeur;
 };
