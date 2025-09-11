e335e98b1cec05f89b71a346416b21b8006c5814
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-11 10:02:50 +0200

    [DEPOSE-JEU] Protège la création de jeu
    ...en vérifiant la demande vienne d'un utilisateur connecté

--- back/src/api/ressourceJeux.ts
+++ back/src/api/ressourceJeux.ts
@@ -1,13 +1,21 @@
 import { Request, Response, Router } from 'express';
 import { ConfigurationServeur } from './dsc';
 
-export const ressourceJeux = ({ entrepotJeux }: ConfigurationServeur) => {
+export const ressourceJeux = ({
+  adaptateurJWT,
+  entrepotJeux,
+}: ConfigurationServeur) => {
   const routeur = Router();
 
-  routeur.post('/', async (_requete: Request, reponse: Response) => {
-    const { nom } = _requete.body;
-    entrepotJeux.ajoute({ nom });
-    reponse.sendStatus(201);
+  routeur.post('/', async (requete: Request, reponse: Response) => {
+    try {
+      adaptateurJWT.decode(requete.session?.token);
+      const { nom } = requete.body;
+      entrepotJeux.ajoute({ nom });
+      reponse.sendStatus(201);
+    } catch {
+      reponse.sendStatus(401);
+    }
   });
   return routeur;
 };
