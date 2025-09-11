eee28c4c2a83a90d8232db72833bc638a7046053
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-11 10:02:50 +0200

    [DEPOSE-JEU] Protège la création de jeu
    ...en vérifiant la demande vienne d'un utilisateur connecté

--- back/tests/api/ressourceJeux.spec.ts
+++ back/tests/api/ressourceJeux.spec.ts
@@ -4,22 +4,46 @@ import { beforeEach, describe, expect, it } from 'vitest';
 import { creeServeur } from '../../src/api/dsc';
 import { EntrepotJeux } from '../../src/metier/entrepotJeux';
 import { EntrepotJeuxMemoire } from '../infra/entrepotJeuxMemoire';
-import { configurationDeTestDuServeur } from './fauxObjets';
+import { configurationDeTestDuServeur, fauxAdaptateurJWT } from './fauxObjets';
+import { encodeSession } from './cookie';
+import { AdaptateurJWT } from '../../src/api/adaptateurJWT';
 
 describe('La ressource des jeux', () => {
   let serveur: Express;
+  let adaptateurJWT: AdaptateurJWT;
   let entrepotJeux: EntrepotJeux;
 
   beforeEach(() => {
     entrepotJeux = new EntrepotJeuxMemoire();
-    serveur = creeServeur({ ...configurationDeTestDuServeur(), entrepotJeux });
+    adaptateurJWT = { ...fauxAdaptateurJWT };
+    serveur = creeServeur({
+      ...configurationDeTestDuServeur(),
+      adaptateurJWT,
+      entrepotJeux,
+    });
   });
 
   describe('sur un POST', () => {
-    it('retourne un 201', async () => {
-      const reponse = await request(serveur).post('/api/jeux').send();
+    it("retourne un 201 si l'utilisateur est connecté", async () => {
+      const cookie = encodeSession({
+        email: 'jeanne.dupont@mail.com',
+      });
+
+      const reponse = await request(serveur)
+        .post('/api/jeux')
+        .set('Cookie', [cookie])
+        .send();
 
       expect(reponse.status).toEqual(201);
+    });
+
+    it("retourne un 401 si il n'y a pas d'utilisateur connecté", async () => {
+      adaptateurJWT.decode = () => {
+        throw new Error('erreur de décodage');
+      };
+      const reponse = await request(serveur).post('/api/jeux').send();
+
+      expect(reponse.status).toEqual(401);
     });
 
     it("ajoute un jeu dans l'entrepot des jeux", async () => {
