6b577a8bfef193104354320a8942824fd61166b4
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-09-11 14:40:21 +0200

    [BUG] Attends l’ajout dans l’entrepôt
    ...avant de rendre la main au niveau de la requête

--- back/tests/infra/entrepotMemoire.ts
+++ back/tests/infra/entrepotMemoire.ts
@@ -1,8 +1,13 @@
 export class EntrepotMemoire<T> {
   entites: T[] = [];
 
-  async ajoute(entite: T) {
-    this.entites.push(entite);
+  async ajoute(entite: T): Promise<void> {
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        this.entites.push(entite);
+        resolve(undefined);
+      }, 10),
+    );
   }
 
   tous = async () => [...this.entites];
