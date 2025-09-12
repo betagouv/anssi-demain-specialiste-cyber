c7e0a632c4845dd19b504ca4cc5d1205f8d2dc14
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-09-12 11:26:47 +0200

    Ajoute un adaptateur pour la journalisation des événements

---
+++ back/src/infra/adaptateurJournal.ts
@@ -0,0 +1,29 @@
+export type DonneesEvenement = DonneesEvenementNouvelUtilisateur;
+
+type DonneesCommunesEvenement = {
+  date: Date;
+};
+
+type DonneesEvenementNouvelUtilisateur = DonneesCommunesEvenement & {
+  donnees: {
+    idUtilisateur: string;
+  };
+  type: 'NOUVEL_UTILISATEUR_INSCRIT';
+};
+
+export type AdaptateurJournal = {
+  consigneEvenement: (donneesEvenement: DonneesEvenement) => Promise<void>;
+};
+
+export const adaptateurJournalMemoire: AdaptateurJournal = {
+  async consigneEvenement(donneesEvenement: DonneesEvenement): Promise<void> {
+    // eslint-disable-next-line no-console
+    console.log(
+      `[JOURNAL MSC] Nouvel évènement \n${JSON.stringify(donneesEvenement)}`,
+    );
+  },
+};
+
+export const fabriqueAdaptateurJournal = () => {
+  return adaptateurJournalMemoire;
+};
