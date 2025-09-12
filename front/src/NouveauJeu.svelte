3dfbdd2a7c3f7bd23158767b4b237c9666fca000
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-09-12 15:10:46 +0200

    Affiche le formulaire de création d’un jeu

---
+++ front/src/NouveauJeu.svelte
@@ -0,0 +1,13 @@
+<svelte:options customElement={{ tag: 'dsc-nouveau-jeu', shadow: 'none' }} />
+
+<script lang="ts">
+</script>
+
+<form>
+  <label>
+    Nom du jeu
+    <input type="text" />
+  </label>
+
+  <button type="submit">Terminer</button>
+</form>
--- front/src/index.ts
+++ front/src/index.ts
@@ -2,3 +2,4 @@ import './index.scss';
 export * from './Catalogue.svelte';
 export * from './ZoneIdentification.svelte';
 export * from './CreationCompte.svelte';
+export * from './NouveauJeu.svelte';
