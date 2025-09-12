b5b701c5bcd76d92184acf949b5ff3e0442df591
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-09-12 15:50:59 +0200

    Soumets la création d’un nouveau jeu
    ...avec son nom qui est requis dans le formulaire

--- front/src/NouveauJeu.svelte
+++ front/src/NouveauJeu.svelte
@@ -1,13 +1,27 @@
 <svelte:options customElement={{ tag: 'dsc-nouveau-jeu', shadow: 'none' }} />
 
 <script lang="ts">
+  import axios from 'axios';
+
+  let nom = $state('');
+
+  const soumets = async (event: SubmitEvent) => {
+    event.preventDefault();
+    await axios.post('/api/jeux', { nom });
+  };
 </script>
 
-<form>
+<form onsubmit={soumets}>
   <label>
     Nom du jeu
-    <input type="text" />
+    <input type="text" bind:value={nom} required />
   </label>
 
   <button type="submit">Terminer</button>
 </form>
+
+<style>
+  input:user-invalid {
+    border: 2px solid red;
+  }
+</style>
