6f5f16e6fd24962dbf91224c521ec18cc3b06198
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-15 15:45:14 +0200

    Ajoute la notion d'enseignant à un jeu

--- back/src/metier/jeu.ts
+++ back/src/metier/jeu.ts
@@ -1,11 +1,22 @@
 import { randomUUID } from 'crypto';
+import { Utilisateur } from './utilisateur';
 
 export class Jeu {
   readonly id: string;
   nom: string;
+  enseignant?: Utilisateur;
 
-  constructor({ id, nom }: { id?: string; nom: string }) {
+  constructor({
+    id,
+    nom,
+    enseignant,
+  }: {
+    id?: string;
+    nom: string;
+    enseignant?: Utilisateur;
+  }) {
     this.id = id ?? randomUUID();
     this.nom = nom;
+    this.enseignant = enseignant;
   }
 }
