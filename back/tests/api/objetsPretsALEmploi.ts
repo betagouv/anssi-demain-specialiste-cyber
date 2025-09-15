87a6420fc9b4fe0aab077d66cc9c2c872e70bb8b
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-15 15:39:45 +0200

    [SOIN] Extrais JeanneDupont dans un objet prêt à l'emploi

---
+++ back/tests/api/objetsPretsALEmploi.ts
@@ -0,0 +1,9 @@
+import { Utilisateur } from '../../src/metier/utilisateur';
+
+export const jeanneDupont = new Utilisateur({
+  email: 'jeanne.dupont@mail.com',
+  infolettreAcceptee: true,
+  prenom: '',
+  nom: '',
+  siretEntite: '',
+});
