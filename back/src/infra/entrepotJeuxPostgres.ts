f2cec1e1420c1014649dc659876f67cd9b9781a4
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-09-15 16:44:01 +0200

    Écris l’enseignant du jeu en base de données

--- back/src/infra/entrepotJeuxPostgres.ts
+++ back/src/infra/entrepotJeuxPostgres.ts
@@ -2,15 +2,24 @@ import Knex from 'knex';
 import config from '../../knexfile';
 import { EntrepotJeux } from '../metier/entrepotJeux';
 import { Jeu } from '../metier/jeu';
+import { AdaptateurHachage } from './adaptateurHachage';
 
 export class EntrepotJeuxPostgres implements EntrepotJeux {
   knex: Knex.Knex;
+  private adaptateurHachage: AdaptateurHachage;
 
-  constructor() {
+  constructor({ adaptateurHachage }: { adaptateurHachage: AdaptateurHachage }) {
+    this.adaptateurHachage = adaptateurHachage;
     this.knex = Knex(config);
   }
   async ajoute(jeu: Jeu): Promise<void> {
-    await this.knex('jeux').insert(jeu);
+    await this.knex('jeux').insert({
+      id: jeu.id,
+      nom: jeu.nom,
+      id_enseignant: jeu.enseignant
+        ? this.adaptateurHachage.hache(jeu.enseignant.email)
+        : null,
+    });
   }
   async tous(): Promise<Jeu[]> {
     return (await this.knex('jeux')).map(
