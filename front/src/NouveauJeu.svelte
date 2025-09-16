83387c82580a7c48e2927b9c8203bf7040adb6d2
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-16 11:34:29 +0200

    [DEPOSE-JEU] Propose la sélection de la séquence
    ... dans le formulaire de dépose de jeu

--- front/src/NouveauJeu.svelte
+++ front/src/NouveauJeu.svelte
@@ -4,10 +4,11 @@
   import axios from 'axios';
 
   let nom = $state('');
+  let sequence = $state('');
 
   const soumets = async (event: SubmitEvent) => {
     event.preventDefault();
-    await axios.post('/api/jeux', { nom });
+    await axios.post('/api/jeux', { nom, sequence });
   };
 </script>
 
@@ -16,6 +17,40 @@
     Nom du jeu
     <input type="text" bind:value={nom} required />
   </label>
+
+  <div class="sequence">
+    <p>Format de la séquence CyberEnJeux</p>
+    <div class="boutons">
+      <label>
+        <input
+          type="radio"
+          name="sequence"
+          value="heure"
+          required
+          bind:group={sequence}
+        />
+        Heure de cours
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="sequence"
+          value="demi-journee"
+          bind:group={sequence}
+        />
+        Demi-journee
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="sequence"
+          value="journee"
+          bind:group={sequence}
+        />
+        Journée
+      </label>
+    </div>
+  </div>
 
   <button type="submit">Terminer</button>
 </form>
@@ -26,6 +61,16 @@
     flex-direction: column;
     margin: 0 auto;
     max-width: 792px;
+
+    .sequence {
+      display: flex;
+      flex-direction: column;
+
+      .boutons {
+        display: flex;
+        flex-direction: column;
+      }
+    }
 
     input:user-invalid {
       border: 2px solid red;
