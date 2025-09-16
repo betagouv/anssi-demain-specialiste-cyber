954f79035482369eb94a75257d2eb2004ba18bd1
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-16 15:42:46 +0200

    Teste la validation du formulaire

--- front/tests/NouveauJeu.spec.ts
+++ front/tests/NouveauJeu.spec.ts
@@ -1,8 +1,20 @@
 import { render } from '@testing-library/svelte/svelte5';
-import { describe, expect, it } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import NouveauJeu from '../src/NouveauJeu.svelte';
 
+const axiosMock = vi.hoisted(() => ({ post: vi.fn() }));
+
+vi.mock('axios', () => {
+  return { default: axiosMock };
+});
+
 describe('Le formulaire de dépose de jeu', () => {
+  const user = userEvent.setup();
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
   describe('propose', () => {
     it('de saisir le nom du jeu', () => {
       const { getByRole } = render(NouveauJeu);
@@ -17,6 +29,32 @@ describe('Le formulaire de dépose de jeu', () => {
       expect(getByRole('radio', { name: 'Heure de cours' })).toBeVisible();
       expect(getByRole('radio', { name: 'Demi-journee' })).toBeVisible();
       expect(getByRole('radio', { name: 'Journée' })).toBeVisible();
+    });
+  });
+
+  describe('lors de la soumission', () => {
+    it("envoie le formulaire à l'api des jeux", async () => {
+      const { getByRole, queryAllByRole } = render(NouveauJeu);
+
+      await user.type(getByRole('textbox', { name: 'Nom du jeu' }), 'TEST');
+      await user.click(getByRole('radio', { name: 'Heure de cours' }));
+      await user.click(getByRole('button', { name: 'Terminer' }));
+
+      expect(axiosMock.post).toHaveBeenCalledExactlyOnceWith('/api/jeux', {
+        nom: 'TEST',
+        sequence: 'heure',
+      });
+      expect(queryAllByRole('alert')).toHaveLength(0);
+    });
+
+    it("n'envoie pas le formulaire si il y a un souci de validation", async () => {
+      const { getByRole, getByText } = render(NouveauJeu);
+
+      await user.click(getByRole('button', { name: 'Terminer' }));
+
+      expect(axiosMock.post).not.toHaveBeenCalled();
+      expect(getByText('Le nom est obligatoire')).toBeVisible();
+      expect(getByText('La séquence est obligatoire')).toBeVisible();
     });
   });
 });
