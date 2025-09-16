89c2de0f08aec6980b2b37b06c23745515420fe1
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-09-16 17:25:52 +0200

    [DEPOSE-JEU] Ajoute la séquence au type Jeu

--- back/src/infra/entrepotJeuxPostgres.ts
+++ back/src/infra/entrepotJeuxPostgres.ts
@@ -21,22 +21,30 @@ export class EntrepotJeuxPostgres implements EntrepotJeux {
     this.entrepotUtilisateur = entrepotUtilisateur;
     this.knex = Knex(config);
   }
+
   async ajoute(jeu: Jeu): Promise<void> {
     await this.knex('jeux').insert({
       id: jeu.id,
       nom: jeu.nom,
+      sequence: jeu.sequence,
       id_enseignant: jeu.enseignant
         ? this.adaptateurHachage.hache(jeu.enseignant.email)
         : null,
     });
   }
+
   async tous(): Promise<Jeu[]> {
     return Promise.all(
       (await this.knex('jeux')).map(async (jeuEnDB) => {
         const enseignant = await this.entrepotUtilisateur.parEmailHache(
           jeuEnDB.id_enseignant,
         );
-        return new Jeu({ id: jeuEnDB.id, nom: jeuEnDB.nom, enseignant });
+        return new Jeu({
+          id: jeuEnDB.id,
+          nom: jeuEnDB.nom,
+          enseignant,
+          sequence: jeuEnDB.sequence,
+        });
       }),
     );
   }
