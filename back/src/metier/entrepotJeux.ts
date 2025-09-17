a85afdf8961311862da397f05a6253d5a2043055
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-09-17 10:59:35 +0200

    Récupère les jeux de l'utilisateur connecté

--- back/src/metier/entrepotJeux.ts
+++ back/src/metier/entrepotJeux.ts
@@ -1,6 +1,8 @@
 import { Jeu } from './jeu';
+import { Utilisateur } from './utilisateur';
 
 export interface EntrepotJeux {
   ajoute(jeu: Jeu): Promise<void>;
   tous(): Promise<Jeu[]>;
+  lesJeuxDe(utilisateur: Utilisateur): Promise<Jeu[]>;
 }
--- back/tests/infra/entrepotJeuxMemoire.ts
+++ back/tests/infra/entrepotJeuxMemoire.ts
@@ -1,7 +1,15 @@
 import { EntrepotJeux } from '../../src/metier/entrepotJeux';
 import { Jeu } from '../../src/metier/jeu';
+import { Utilisateur } from '../../src/metier/utilisateur';
 import { EntrepotMemoire } from './entrepotMemoire';
 
 export class EntrepotJeuxMemoire
   extends EntrepotMemoire<Jeu>
-  implements EntrepotJeux {}
+  implements EntrepotJeux
+{
+  async lesJeuxDe(utilisateur: Utilisateur): Promise<Jeu[]> {
+    return this.entites.filter(
+      (jeu) => jeu.enseignant && jeu.enseignant.email === utilisateur.email,
+    );
+  }
+}
