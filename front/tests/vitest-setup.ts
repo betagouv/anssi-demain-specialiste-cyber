06d426ad08419f95e16c7457d536feac7a96ed44
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-18 09:55:53 +0200

    Importe l'ui-kit en dépendance
    ...afin de pouvoir rendre les web components dans un context de test

---
+++ front/tests/vitest-setup.ts
@@ -0,0 +1,2 @@
+import '@testing-library/jest-dom';
+import '@lab-anssi/ui-kit/dist/webcomponents/lab-anssi-ui-kit.iife';
--- front/vitest.config.ts
+++ front/vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest-setup.ts',
+    setupFiles: './tests/vitest-setup.ts',
     include: ['tests/**/*.{test,spec}.{js,ts}'],
   },
 });
