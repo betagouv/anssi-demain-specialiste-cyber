025740253a1c02e7c9ba0907007c6fc0376b5e95
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-09-19 10:18:08 +0200

    [DEPOSE-JEU] Ajoute la colonne "eleves" à la table jeux

---
+++ back/migrations/20250919081506_ajouteColonneElevesAJeu.ts
@@ -0,0 +1,13 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('jeux', (table) => {
+    table.jsonb('eleves');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('jeux', (table) => {
+    table.dropColumn('eleves');
+  });
+}
