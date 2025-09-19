ed002a8b4b4179f7abe3dc5de0cd207d0e080cbd
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-19 11:38:50 +0200

    [SOIN][BUG] Type la conversion métier/BDD
    ...de l'entrepot jeux postgres. Et corrige au passe un mauvais nommage sur le nom de l'etablissement

--- back/src/infra/entrepotJeuxPostgres.ts
+++ back/src/infra/entrepotJeuxPostgres.ts
@@ -1,10 +1,28 @@
 import Knex from 'knex';
 import config from '../../knexfile';
 import { EntrepotJeux } from '../metier/entrepotJeux';
-import { Jeu } from '../metier/jeu';
-import { AdaptateurHachage } from './adaptateurHachage';
 import { EntrepotUtilisateur } from '../metier/entrepotUtilisateur';
+import { Jeu } from '../metier/jeu';
+import { Classe } from '../metier/referentiels/classes';
+import { Discipline } from '../metier/referentiels/disciplines';
+import { Sequence } from '../metier/referentiels/sequence';
 import { Utilisateur } from '../metier/utilisateur';
+import { AdaptateurHachage } from './adaptateurHachage';
+
+type JeuEnDB = {
+  id: string;
+  nom: string;
+  sequence: string;
+  id_enseignant: string | null;
+  nom_etablissement: string;
+  classe: string;
+  discipline: string;
+  eleves: string[];
+};
+
+type JeuEnDBInsertion = Omit<JeuEnDB, 'eleves'> & {
+  eleves: string; // IMPORTANT, Knex ne sait pas gérer l'insertion de tableau directement, cf : https://knexjs.org/guide/schema-builder.html#json
+};
 
 export class EntrepotJeuxPostgres implements EntrepotJeux {
   knex: Knex.Knex;
@@ -34,51 +52,39 @@ export class EntrepotJeuxPostgres implements EntrepotJeux {
       nom_etablissement: jeu.nomEtablissement,
       classe: jeu.classe,
       discipline: jeu.discipline,
-      eleves: JSON.stringify(jeu.eleves), // IMPORTANT, Knex ne sait pas gérer l'insertion de tableau directement, cf : https://knexjs.org/guide/schema-builder.html#json
-    });
+      eleves: JSON.stringify(jeu.eleves),
+    } satisfies JeuEnDBInsertion);
   }
 
   async tous(): Promise<Jeu[]> {
     return Promise.all(
-      (await this.knex('jeux')).map(async (jeuEnDB) => {
-        const enseignant = await this.entrepotUtilisateur.parEmailHache(
-          jeuEnDB.id_enseignant,
-        );
-        return new Jeu({
-          id: jeuEnDB.id,
-          nom: jeuEnDB.nom,
-          enseignant,
-          sequence: jeuEnDB.sequence,
-          nomEtablissement: jeuEnDB.nomEtablissement,
-          classe: jeuEnDB.classe,
-          discipline: jeuEnDB.discipline,
-          eleves: jeuEnDB.eleves ?? [],
-        });
-      }),
+      (await this.knex('jeux')).map(this.donneesEnDbVersMetier),
     );
   }
 
   async lesJeuxDe(utilisateur: Utilisateur): Promise<Jeu[]> {
     return Promise.all(
       (
-        await this.knex('jeux').where({
+        await this.knex<JeuEnDB>('jeux').where({
           id_enseignant: this.adaptateurHachage.hache(utilisateur.email),
         })
-      ).map(async (jeuEnDB) => {
-        const enseignant = await this.entrepotUtilisateur.parEmailHache(
-          jeuEnDB.id_enseignant,
-        );
-        return new Jeu({
-          id: jeuEnDB.id,
-          nom: jeuEnDB.nom,
-          enseignant,
-          sequence: jeuEnDB.sequence,
-          nomEtablissement: jeuEnDB.nomEtablissement,
-          classe: jeuEnDB.classe,
-          discipline: jeuEnDB.discipline,
-          eleves: jeuEnDB.eleves ?? [],
-        });
-      }),
+      ).map(this.donneesEnDbVersMetier),
     );
+  }
+
+  private async donneesEnDbVersMetier(jeuEnDB: JeuEnDB): Promise<Jeu> {
+    const enseignant = jeuEnDB.id_enseignant
+      ? await this.entrepotUtilisateur.parEmailHache(jeuEnDB.id_enseignant)
+      : undefined;
+    return new Jeu({
+      id: jeuEnDB.id,
+      nom: jeuEnDB.nom,
+      enseignant,
+      sequence: jeuEnDB.sequence as Sequence,
+      nomEtablissement: jeuEnDB.nom_etablissement,
+      classe: jeuEnDB.classe as Classe,
+      discipline: jeuEnDB.discipline as Discipline,
+      eleves: jeuEnDB.eleves ?? [],
+    });
   }
 }
