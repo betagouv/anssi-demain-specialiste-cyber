18fd0e4227210f74f344bd81abcf9c73adb1adba
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-19 13:28:42 +0200

    Expose les matchers textuels shadow dom
    ...équivalent aux matchers textuels natifs

--- front/tests/shadow-dom-utilitaires.ts
+++ front/tests/shadow-dom-utilitaires.ts
@@ -1,9 +1,13 @@
 import {
   type AllByRole,
+  type AllByText,
   buildQueries,
   type ByRoleMatcher,
   type ByRoleOptions,
+  type Matcher,
+  type MatcherOptions,
   queryAllByRole,
+  queryAllByText,
 } from '@testing-library/dom';
 
 const _queryAllByRoleDeep: AllByRole = function (container, ...rest) {
@@ -12,6 +16,22 @@ const _queryAllByRoleDeep: AllByRole = function (container, ...rest) {
     if (element.shadowRoot) {
       result.push(
         ..._queryAllByRoleDeep(
+          element.shadowRoot as ParentNode as HTMLElement,
+          ...rest,
+        ),
+      );
+    }
+  }
+
+  return result;
+};
+
+const _queryAllByTextDeep: AllByText = function (container, ...rest) {
+  const result = queryAllByText(container, ...rest); // replace here with different queryAll* variants.
+  for (const element of container.querySelectorAll('*')) {
+    if (element.shadowRoot) {
+      result.push(
+        ..._queryAllByTextDeep(
           element.shadowRoot as ParentNode as HTMLElement,
           ...rest,
         ),
@@ -34,9 +54,28 @@ const [
   (_, role) => `Unable to find an element with the role ${role}`,
 );
 
+const [
+  _queryByTextDeep,
+  _getAllByTextDeep,
+  _getByTextDeep,
+  _findAllByTextDeep,
+  _findByTextDeep,
+] = buildQueries<[Matcher, MatcherOptions?]>(
+  _queryAllByTextDeep,
+  (_, role) => `Found multiple elements with the role ${role}`,
+  (_, role) => `Unable to find an element with the role ${role}`,
+);
+
 export const queryAllByRoleDeep = _queryAllByRoleDeep.bind(null, document.body);
 export const queryByRoleDeep = _queryByRoleDeep.bind(null, document.body);
 export const getAllByRoleDeep = _getAllByRoleDeep.bind(null, document.body);
 export const getByRoleDeep = _getByRoleDeep.bind(null, document.body);
 export const findAllByRoleDeep = _findAllByRoleDeep.bind(null, document.body);
 export const findByRoleDeep = _findByRoleDeep.bind(null, document.body);
+
+export const queryAllByTextDeep = _queryAllByTextDeep.bind(null, document.body);
+export const queryByTextDeep = _queryByTextDeep.bind(null, document.body);
+export const getAllByTextDeep = _getAllByTextDeep.bind(null, document.body);
+export const getByTextDeep = _getByTextDeep.bind(null, document.body);
+export const findAllByTextDeep = _findAllByTextDeep.bind(null, document.body);
+export const findByTextDeep = _findByTextDeep.bind(null, document.body);
