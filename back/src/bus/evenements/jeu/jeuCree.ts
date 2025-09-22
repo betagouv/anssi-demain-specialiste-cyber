b0b4a369d84605ced88750fd539b9af5dc61f68e
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-22 16:38:08 +0200

    [SOIN] Type fortement l'eévénement JeuCree

--- back/src/bus/evenements/jeu/jeuCree.ts
+++ back/src/bus/evenements/jeu/jeuCree.ts
@@ -1,11 +1,15 @@
+import { Classe } from '../../../metier/referentiels/classes';
+import { Discipline } from '../../../metier/referentiels/disciplines';
+import { Sequence } from '../../../metier/referentiels/sequence';
+
 export class JeuCree {
   constructor(
     public readonly emailAuteur: string,
     public readonly nom: string,
-    public readonly sequence: string,
+    public readonly sequence: Sequence,
     public readonly nomEtablissement: string,
-    public readonly classe: string,
-    public readonly discipline: string,
+    public readonly classe: Classe,
+    public readonly discipline: Discipline,
     public readonly nombreEleves: number,
   ) {}
 }
