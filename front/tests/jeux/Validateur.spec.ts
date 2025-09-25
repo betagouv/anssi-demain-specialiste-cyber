97d3de3214a129d90f11f773ea74235b47dbf442
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-09-25 15:59:38 +0200

    [SOIN] Extrais le mapping des erreurs zod
    ...dans une méthode générique

---
+++ front/tests/jeux/Validateur.spec.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from 'vitest';
+import { extracteurErreursZod } from '../../src/validateur';
+import { ZodError } from 'zod';
+
+describe("L'extracteur d'erreur zod", () => {
+  type MonType = {
+    champBool: boolean;
+    champText: string;
+    champNumeric: number;
+  };
+
+  it("retourne un objet vide si il n'y a pas d'erreur", () => {
+    const erreursZod: ZodError<MonType>['issues'] = [];
+    expect(extracteurErreursZod<MonType>(erreursZod)).toEqual({});
+  });
+
+  it("retourne un objet avec le champ en erreur en clé, et le message d'erreur en valeur", () => {
+    const erreursZod: ZodError<MonType>['issues'] = [
+      {
+        path: ['champBool'],
+        code: 'invalid_type',
+        message: 'Invalid input: expected boolean',
+        expected: 'boolean',
+      },
+      {
+        path: ['champNumeric'],
+        code: 'invalid_type',
+        message: 'Invalid input: expected number',
+        expected: 'number',
+      },
+    ];
+    expect(extracteurErreursZod<MonType>(erreursZod)).toEqual({
+      champBool: 'Invalid input: expected boolean',
+      champNumeric: 'Invalid input: expected number',
+    });
+  });
+});
