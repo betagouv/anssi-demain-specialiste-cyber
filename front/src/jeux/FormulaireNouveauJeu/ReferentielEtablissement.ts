45c6dcb1afa0b1d924f37e53b5da8a0cfe695612
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-10-01 14:47:44 +0200

    [DEPOSE-JEU] Propose des établissements
    ...en recherchant dans l'annuaire de l'éducation nationale

--- front/src/jeux/FormulaireNouveauJeu/ReferentielEtablissement.ts
+++ front/src/jeux/FormulaireNouveauJeu/ReferentielEtablissement.ts
@@ -1,3 +1,36 @@
+import axios from 'axios';
+
 export interface ReferentielEtablissement {
   trouveParNom: (nom: string) => Promise<string[]>;
+}
+
+export class AdaptateurAnnuaireEducationNationale
+  implements ReferentielEtablissement
+{
+  constructor() {}
+
+  async trouveParNom(nom: string) {
+    const reponse = await axios.get(
+      'https://data.education.gouv.fr/api/v2/catalog/datasets/fr-en-annuaire-education/records',
+      {
+        params: {
+          select: 'nom_etablissement, code_departement',
+          where: `nom_etablissement like '*${nom}*'`,
+          limit: '20',
+          offset: '0',
+        },
+      },
+    );
+
+    const resultats = reponse.data.records.map(
+      (record: {
+        record: {
+          fields: { nom_etablissement: string; code_departement: string };
+        };
+      }) =>
+        `${record.record.fields.nom_etablissement} (${Number(record.record.fields.code_departement)})`,
+    );
+
+    return resultats;
+  }
 }
