f6192d407e2ba72fadff190ae1600b83c832564b
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-10-03 11:48:36 +0200

    [FICHE-JEU] Mets en forme le menu latéral
    ...en mode tablette

--- front/src/jeux/FicheJeu.svelte
+++ front/src/jeux/FicheJeu.svelte
@@ -52,6 +52,8 @@
     <a href="#">Voir le fil d'Ariane</a>
   </nav>
   <div class="cartouche">
+    <dsfr-badge label="Thématique cyber" accent="purple-glycine" type="accent"
+    ></dsfr-badge>
     <p class="titre-alternatif-xs">CyberCluedo</p>
     <p class="elaboration">
       Élaboré par Cindy, Kylian, Kevin et Branda du Lycée Françoise de
@@ -64,72 +66,76 @@
     />
   </div>
 </div>
-<dsfr-side-menu
-  title=""
-  {items}
-  buttonLabel="Dans cette fiche"
-  buttonId="menu-fiche-jeu"
-  hasTitle={false}
-></dsfr-side-menu>
 
-<dsfr-container>
-  <div class="contenu">
-    <section id="infos-generales">
-      <h2>Informations générales</h2>
-      <ul>
-        <li>
-          <span>Format</span>
-          <span>Demi journée </span>
-        </li>
-        <li>
-          <span>Établissement</span>
-          <span>Lycée Françoise de Tournefeuille (31)</span>
-        </li>
-        <li>
-          <span>Enseignant&middot;e</span>
-          <span>Delphine</span>
-        </li>
-        <li>
-          <span>Élèves</span>
-          <span>Cindy, Kilian, Kevin et Brenda</span>
-        </li>
-        <li>
-          <span>Discipline</span>
-          <span>Sciences économiques et sociales</span>
-        </li>
-        <li>
-          <span>Classe</span>
-          <span>Seconde</span>
-        </li>
-      </ul>
-    </section>
+<div class="corps-de-fiche">
+  <dsfr-side-menu
+    title=""
+    {items}
+    buttonLabel="Dans cette fiche"
+    buttonId="menu-fiche-jeu"
+    hasTitle={false}
+  ></dsfr-side-menu>
 
-    <section id="presentation">
-      <h2>Présentation du jeu</h2>
-      <p>
-        Inspirés du célèbre jeu de société, mes élèves ont imaginé Cyber Cluedo,
-        un jeu d’enquête autour d’une cyberattaque fictive. Le but ? Découvrir
-        qui a commis l’attaque, avec quel outil (clé USB piégée, phishing, etc.)
-        et dans quel service (CDI, administration, salle informatique…). Ce
-        projet leur a permis de réfléchir aux menaces numériques tout en
-        mobilisant leur créativité, leur logique et leur esprit d’équipe. Une
-        façon ludique et concrète d’aborder les enjeux de cybersécurité en
-        classe.
-      </p>
-    </section>
+  <div class="sections">
+    <div class="contenu">
+      <section id="infos-generales">
+        <h2>Informations générales</h2>
+        <ul>
+          <li>
+            <span>Format</span>
+            <span>Demi journée </span>
+          </li>
+          <li>
+            <span>Établissement</span>
+            <span>Lycée Françoise de Tournefeuille (31)</span>
+          </li>
+          <li>
+            <span>Enseignant&middot;e</span>
+            <span>Delphine</span>
+          </li>
+          <li>
+            <span>Élèves</span>
+            <span>Cindy, Kilian, Kevin et Brenda</span>
+          </li>
+          <li>
+            <span>Discipline</span>
+            <span>Sciences économiques et sociales</span>
+          </li>
+          <li>
+            <span>Classe</span>
+            <span>Seconde</span>
+          </li>
+        </ul>
+      </section>
 
-    <section id="photos">
-      <h2>Photos</h2>
-      <div class="photos">
-        <img src="/assets/images/image-generique.svg" alt="generique" />
-        <img src="/assets/images/image-generique.svg" alt="generique" />
-        <img src="/assets/images/image-generique.svg" alt="generique" />
-      </div>
-    </section>
+      <section id="presentation">
+        <h2>Présentation du jeu</h2>
+        <p>
+          Inspirés du célèbre jeu de société, mes élèves ont imaginé Cyber
+          Cluedo, un jeu d’enquête autour d’une cyberattaque fictive. Le but ?
+          Découvrir qui a commis l’attaque, avec quel outil (clé USB piégée,
+          phishing, etc.) et dans quel service (CDI, administration, salle
+          informatique…). Ce projet leur a permis de réfléchir aux menaces
+          numériques tout en mobilisant leur créativité, leur logique et leur
+          esprit d’équipe. Une façon ludique et concrète d’aborder les enjeux de
+          cybersécurité en classe.
+        </p>
+      </section>
+
+      <section id="photos">
+        <h2>Photos</h2>
+        <div class="photos">
+          <img src="/assets/images/image-generique.svg" alt="generique" />
+          <img src="/assets/images/image-generique.svg" alt="generique" />
+          <img src="/assets/images/image-generique.svg" alt="generique" />
+        </div>
+      </section>
+    </div>
+
+    <lab-anssi-temoignages titre="Témoignages" {temoignages} id="temoignages"
+    ></lab-anssi-temoignages>
   </div>
-</dsfr-container>
-<lab-anssi-temoignages titre="Témoignages" {temoignages} id="temoignages"
-></lab-anssi-temoignages>
+</div>
 
 <style lang="scss">
   @use '../points-de-rupture' as *;
@@ -138,7 +144,8 @@
     padding: 1rem 1rem 0;
     background: var(--blue-france-975-sun-113)
       url('/assets/images/hero-fiche-jeu.svg') no-repeat top right -300px;
-    background-size: cover;
+    background-size: auto 100%;
+
     display: flex;
     flex-direction: column;
 
@@ -154,6 +161,18 @@
       .titre-alternatif-xs {
         margin-top: 1.5rem;
         color: var(--grey-50-1000);
+
+        @include a-partir-de(md) {
+          margin-top: 0.25rem;
+        }
+      }
+
+      dsfr-badge {
+        display: none;
+        margin-top: 1.5rem;
+        @include a-partir-de(md) {
+          display: block;
+        }
       }
 
       .elaboration {
@@ -165,49 +184,88 @@
     .illustration-jeu {
       background: white;
       padding: 0.25rem 0.25rem 0;
-      max-width: 18rem;
+      max-width: 36.75rem;
       align-self: center;
+
       img {
         width: 100%;
       }
     }
   }
 
-  .contenu {
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-    section {
-      ul {
-        display: flex;
-        flex-direction: column;
-        gap: 0.5rem;
-        li {
-          span:nth-child(1) {
-            &::after {
-              content: '\00a0:\00a0';
-            }
-          }
-          span:nth-child(2) {
-            font-weight: bold;
-          }
-        }
-      }
+  .corps-de-fiche {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-areas: 'menu' 'sections';
+    padding-left: 1rem;
+    padding-right: 1rem;
+    max-width: 78rem;
+    margin: 0 auto;
 
-      .photos {
-        display: grid;
-        gap: 1rem;
-        grid-template-columns: 1fr;
-        img {
-          aspect-ratio: 4/3;
-          width: 100%;
-          background-color: var(--blue-france-975-sun-113);
-        }
+    @include a-partir-de(md) {
+      grid-template-columns: 11.75rem 1fr;
+      grid-template-areas: 'menu sections';
+      padding-top: 1.5rem;
+    }
+
+    @include a-partir-de(lg) {
+      padding-left: 1.5rem;
+      padding-right: 1.5rem;
+    }
+
+    dsfr-side-menu {
+      @include a-partir-de(md) {
+        height: 100%;
       }
     }
-  }
-  lab-anssi-temoignages {
-    display: block;
-    margin-top: -44px;
+
+    .sections {
+      grid-area: sections;
+      @include a-partir-de(md) {
+        padding-top: 0.75rem;
+      }
+      .contenu {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        section {
+          ul {
+            display: flex;
+            flex-direction: column;
+            gap: 0.5rem;
+            li {
+              span:nth-child(1) {
+                &::after {
+                  content: '\00a0:\00a0';
+                }
+              }
+              span:nth-child(2) {
+                font-weight: bold;
+              }
+            }
+          }
+
+          .photos {
+            display: grid;
+            gap: 1rem;
+            grid-template-columns: 1fr;
+
+            @include a-partir-de(md) {
+              grid-template-columns: 1fr 1fr;
+            }
+
+            img {
+              aspect-ratio: 4/3;
+              width: 100%;
+              background-color: var(--blue-france-975-sun-113);
+            }
+          }
+        }
+      }
+      lab-anssi-temoignages {
+        display: block;
+        margin-top: -44px;
+      }
+    }
   }
 </style>
