eba1fe704c24ccbc9d8353cd7cfc3e8680905853
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-10-03 16:32:22 +0200

    [CEJ] Rajoute l'encart mise en avant

---
+++ front/src/MiseEnAvant.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  type Lien = {
+    lien: string;
+    libelle: string;
+    detail?: string;
+    telechargement?: boolean;
+  };
+
+  type Props = {
+    titre: string;
+    children: Snippet;
+    liens: Lien[];
+    illustration: { lien: string; alt?: string };
+  };
+
+  const { titre, liens, illustration, children }: Props = $props();
+</script>
+
+<section class="mise-en-avant fonce">
+  <h2>{titre}</h2>
+  {@render children?.()}
+  <ul>
+    {#each liens as { lien, libelle, detail, telechargement }}
+      <li>
+        <lab-anssi-lien
+          titre={libelle}
+          cible={lien}
+          apparence="lien-texte"
+          taille="sm"
+          positionIcone="droite"
+          icone={telechargement ? 'download-line' : undefined}
+        ></lab-anssi-lien>
+        {#if detail}
+          <div class="detail">{detail}</div>
+        {/if}
+      </li>
+    {/each}
+  </ul>
+  <img src={illustration.lien} alt={illustration.alt} />
+</section>
+
+<style lang="scss">
+  @use './points-de-rupture' as *;
+
+  .mise-en-avant {
+    display: grid;
+    color: #ffffff;
+
+    background:
+      url('/assets/images/cercles.svg') no-repeat,
+      #131429;
+    background-size: auto 100%;
+    background-position: 25% bottom;
+
+    @media (min-aspect-ratio: 1/1) {
+      background-size: auto auto;
+      background-position: right center;
+    }
+
+    padding: 3rem 1rem;
+
+    h2 {
+      font-size: 1.75rem;
+      line-height: 2.25rem;
+      margin: 0 0 1rem;
+    }
+
+    :global p {
+      margin: 0;
+    }
+
+    ul {
+      list-style-type: disc;
+      padding-left: 1rem;
+      margin: 1.5rem 0 2rem;
+
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+
+      li {
+        .detail {
+          margin-top: 0.25rem;
+          font-size: 0.75rem;
+          line-height: 1.25rem;
+        }
+      }
+    }
+
+    img {
+      max-width: min(240px, 100%);
+      justify-self: center;
+    }
+
+    @include a-partir-de(lg) {
+      padding: 3rem;
+
+      grid-template-rows: repeat(3, auto);
+      grid-template-columns: 3fr 2fr;
+      gap: 0 1.5rem;
+
+      h2 {
+        grid-column: 1;
+      }
+
+      :global p {
+        grid-column: 1;
+      }
+
+      ul {
+        grid-column: 1;
+      }
+
+      img {
+        grid-column: 2;
+        grid-row: 1 / span 3;
+      }
+    }
+  }
+</style>
