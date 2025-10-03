c58316d30b4a489f2ad9ac71d08ccae2e6398204
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-10-03 17:09:08 +0200

    [FICHE-JEU] Expose la ressource api du jeu

---
+++ back/src/api/ressourceJeu.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { ConfigurationServeur } from './configurationServeur';
+
+export const ressourceJeu = ({ entrepotJeux }: ConfigurationServeur) => {
+  const routeur = Router();
+
+  routeur.get('/:id', async (requete, reponse) => {
+    const jeu = await entrepotJeux.parId(requete.params.id);
+    if (!jeu) {
+      reponse.sendStatus(404);
+      return;
+    }
+    reponse.send(jeu);
+  });
+
+  return routeur;
+};
