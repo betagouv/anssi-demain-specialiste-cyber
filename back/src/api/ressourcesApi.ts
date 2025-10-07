ae867057b3df33ad458840c713ebb26cd76b91f2
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-10-07 17:39:11 +0200

    [WIP] Pour rebasage

--- back/src/api/ressourcesApi.ts
+++ back/src/api/ressourcesApi.ts
@@ -1,21 +1,21 @@
 import { Router } from 'express';
 import { ConfigurationServeur } from './configurationServeur';
 import { ressourceJeu } from './ressourceJeu';
-import { ressourceMesJeux } from './ressourceMesJeux';
+import { ressourceJeux } from './ressourceJeux';
 import { ressourceProfil } from './ressourceProfil';
 import { ressourceRessourceCyber } from './ressourceRessourcesCyber';
 import { ressourceUtilisateurs } from './ressourceUtilisateurs';
-import { ressourceJeux } from './ressourceJeux';
+import { ressourceJeuxTemp } from './ressourceJeuxTemp';
 
 export const ressourcesApi = (configurationServeur: ConfigurationServeur) => {
   const router = Router();
 
   router.use('/profil', ressourceProfil(configurationServeur));
   router.use('/utilisateurs', ressourceUtilisateurs(configurationServeur));
-  router.use('/mes-jeux', ressourceMesJeux(configurationServeur));
+  router.use('/mes-jeux', ressourceJeux(configurationServeur));
   router.use(
     '/jeux',
-    ressourceJeux(configurationServeur),
+    ressourceJeuxTemp(configurationServeur),
     ressourceJeu(configurationServeur),
   );
   router.use(
