156aa3f952548e8a4923e1a8fe8f54ba6a9fa8db
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-10-07 18:09:54 +0200

    [REMANIEMENT] Renomme les fichiers pour rebasage
    … suite au renommage des ressources sur main

--- back/src/api/ressourcesApi.ts
+++ back/src/api/ressourcesApi.ts
@@ -1,21 +1,21 @@
 import { Router } from 'express';
 import { ConfigurationServeur } from './configurationServeur';
 import { ressourceJeu } from './ressourceJeu';
-import { ressourceJeux } from './ressourceJeux';
+import { ressourceMesJeux } from './ressourceMesJeux';
 import { ressourceProfil } from './ressourceProfil';
 import { ressourceRessourceCyber } from './ressourceRessourcesCyber';
 import { ressourceUtilisateurs } from './ressourceUtilisateurs';
-import { ressourceJeuxTemp } from './ressourceJeuxTemp';
+import { ressourceJeux } from './ressourceJeux';
 
 export const ressourcesApi = (configurationServeur: ConfigurationServeur) => {
   const router = Router();
 
   router.use('/profil', ressourceProfil(configurationServeur));
   router.use('/utilisateurs', ressourceUtilisateurs(configurationServeur));
-  router.use('/mes-jeux', ressourceJeux(configurationServeur));
+  router.use('/mes-jeux', ressourceMesJeux(configurationServeur));
   router.use(
     '/jeux',
-    ressourceJeuxTemp(configurationServeur),
+    ressourceJeux(configurationServeur),
     ressourceJeu(configurationServeur),
   );
   router.use(
