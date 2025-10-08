7170580a3ac99c98b6b6513018e184361290c34a
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-10-08 17:24:47 +0200

    [DEPOSE-JEU] Valide les données photos d'un jeu

---
+++ front/src/jeux/ValidateurPhotosDuJeu.ts
@@ -0,0 +1,42 @@
+import z from 'zod';
+import { extracteurErreursZod, type Validateur } from '../validateur';
+import type {
+  ErreursValidationJeuEnEdition,
+  JeuEnEdition,
+  PhotosDuJeu,
+} from './jeu';
+
+export class ValidateurPhotosDuJeu implements Validateur<PhotosDuJeu> {
+  schema: z.ZodObject;
+
+  constructor() {
+    this.schema = z.object({
+      photos: z.object(
+        {
+          couverture: z.instanceof(Blob, {
+            error: 'Cette information est obligatoire',
+          }),
+        },
+        { error: 'Cette information est obligatoire' },
+      ),
+    });
+  }
+  valide(jeu: JeuEnEdition): ErreursValidationJeuEnEdition {
+    try {
+      this.schema.parse(jeu);
+      return {};
+    } catch (e) {
+      const zodError = e as z.ZodError;
+      return extracteurErreursZod<PhotosDuJeu>(zodError.issues);
+    }
+  }
+
+  estValide(jeu: JeuEnEdition): boolean {
+    try {
+      this.schema.parse(jeu);
+      return true;
+    } catch {
+      return false;
+    }
+  }
+}
