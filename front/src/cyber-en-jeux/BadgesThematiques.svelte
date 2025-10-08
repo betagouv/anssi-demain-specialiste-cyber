974b62d74979537f0ed240977cf0259f7c6be9f2
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-10-08 15:22:08 +0200

    [SOIN] Crée un composant pour les badges des thématiques

---
+++ front/src/cyber-en-jeux/BadgesThematiques.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { libelleThematique } from '../jeux/thematiques';
+
+  type Props = {
+    taille: 'sm' | 'md';
+    thematiques: string[];
+  };
+
+  const couleursDeBadge = [
+    'purple-glycine',
+    'green-tilleul-verveine',
+    'green-archipel',
+    'green-emeraude',
+    'pink-tuile',
+    'blue-cumulus',
+    'beige-gris-galet',
+    'blue-ecume',
+    'green-bourgeon',
+    'brown-cafe-creme',
+    'orange-terre-battue',
+    'pink-macaron',
+    'yellow-tournesol',
+    'brown-caramel',
+    'green-menthe',
+    'brown-opera',
+    'yellow-moutarde',
+  ];
+
+  const { taille, thematiques }: Props = $props();
+</script>
+
+<dsfr-badges-group
+  badges={thematiques.map((thematique, index) => ({
+    label: libelleThematique(thematique),
+    accent: couleursDeBadge[index % couleursDeBadge.length],
+  }))}
+  size={taille}
+></dsfr-badges-group>
+
+<style lang="scss">
+</style>
