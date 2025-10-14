a51f05b250d08f66982e993761b987b9fb00a11a
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-10-14 16:54:23 +0200

    [CORRECTION] Corrige les tableaux vides en provenance de Grist

--- back/src/infra/entrepotMetiersGrist.ts
+++ back/src/infra/entrepotMetiersGrist.ts
@@ -49,14 +49,15 @@ export class EntrepotMetiersGrist
       titre: enregistrement.fields.Titre_de_la_fiche,
       description: enregistrement.fields.En_quoi_consiste_le_metier_,
       missionPrincipale: enregistrement.fields.Mission_principale,
-      postures: enregistrement.fields.Postures.slice(1),
-      formationsCibles: enregistrement.fields.Formation_cible.slice(1),
-      preRequis: enregistrement.fields.Autre_prerequis_possibles.slice(1),
+      postures: enregistrement.fields.Postures?.slice(1) ?? [],
+      formationsCibles: enregistrement.fields.Formation_cible?.slice(1) ?? [],
+      preRequis:
+        enregistrement.fields.Autre_prerequis_possibles?.slice(1) ?? [],
       remuneration: {
         junior: enregistrement.fields.Salaire_junior_annuel_brut_,
         senior: enregistrement.fields.Salaire_senior_annuel_brut_,
       },
-      metiersProches: enregistrement.fields.Metiers_proches.slice(1),
+      metiersProches: enregistrement.fields.Metiers_proches?.slice(1) ?? [],
       liens: {
         dataemploi: enregistrement.fields.Fiche_data_emploi,
         metierscope: enregistrement.fields.Fiche_metierscope,
