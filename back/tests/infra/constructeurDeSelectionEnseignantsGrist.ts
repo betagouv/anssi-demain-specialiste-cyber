235cc314e202a839afd92fd6393d24f0d865e7d8
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-10-16 16:55:30 +0200

    [ENSEIGNANTS] Récupère les sélections enseignants depuis Grist

---
+++ back/tests/infra/constructeurDeSelectionEnseignantsGrist.ts
@@ -0,0 +1,58 @@
+import { ReponseGrist } from '../../src/infra/entrepotGrist';
+import { SelectionEnseignantsGrist } from '../../src/infra/entrepotSelectionEnseignantsGrist';
+import { ConstructeurLigneGrist, LigneGrist } from './grist';
+
+export type LigneSelectionEnseignant = LigneGrist & SelectionEnseignantsGrist;
+
+export class ConstructeurLigneGristSelectionEnseignants extends ConstructeurLigneGrist<LigneSelectionEnseignant> {
+  avecColonneId(id: string): ConstructeurLigneGristSelectionEnseignants {
+    this.avecColonneEtValeur('Id2', id);
+    return this;
+  }
+
+  avecColonneTitre(titre: string): ConstructeurLigneGristSelectionEnseignants {
+    this.avecColonneEtValeur('Titre', titre);
+    return this;
+  }
+
+  avecColonneExplication(
+    titre: string,
+  ): ConstructeurLigneGristSelectionEnseignants {
+    this.avecColonneEtValeur('Explication', titre);
+    return this;
+  }
+
+  avecColonneRessources(
+    ressources: string[],
+  ): ConstructeurLigneGristSelectionEnseignants {
+    this.avecColonneEtListeDeValeurs('Ressources2', ressources);
+    return this;
+  }
+
+  avecColonneCouleurDeFond(
+    couleur: string,
+  ): ConstructeurLigneGristSelectionEnseignants {
+    this.avecColonneEtValeur('Couleur_de_fond', couleur);
+    return this;
+  }
+}
+
+export class ConstructeurReponseSelectionEnseignantsGrist {
+  private _lignes: LigneSelectionEnseignant[] = [];
+
+  ajouteUneLigne(
+    ligne: LigneSelectionEnseignant,
+  ): ConstructeurReponseSelectionEnseignantsGrist {
+    this._lignes.push(ligne);
+    return this;
+  }
+
+  construis(): ReponseGrist<SelectionEnseignantsGrist> {
+    return {
+      records: this._lignes.map((l) => ({
+        id: l.id,
+        fields: l.fields,
+      })),
+    };
+  }
+}
