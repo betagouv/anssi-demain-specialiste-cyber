97548785f3349f0e5267ef1ab9f83740d4090a8c
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-10-16 18:00:39 +0200

    [SOIN] Rends explicite que les ressources sont typées

--- back/tests/infra/entrepotSelectionEnseignantsMemoire.ts
+++ back/tests/infra/entrepotSelectionEnseignantsMemoire.ts
@@ -1,26 +1,7 @@
 import { EntrepotSelectionEnseignants } from '../../src/metier/entrepotSelectionEnseignants';
 import { Selection } from '../../src/metier/selection';
 import { EntrepotMemoire } from './entrepotMemoire';
-import { EntrepotRessourcesCyberMemoire } from './entrepotRessourceCyberMemoire';
 
 export class EntrepotSelectionEnseignantsMemoire
   extends EntrepotMemoire<Selection>
-  implements EntrepotSelectionEnseignants
-{
-  constructor(private entrepotRessourcesCyber: EntrepotRessourcesCyberMemoire) {
-    super();
-  }
-
-  override async tous(): Promise<Selection[]> {
-    const selections = await super.tous();
-    const ressources = await this.entrepotRessourcesCyber.tous();
-
-    selections.forEach((selection) => {
-      selection.ressources = ressources.filter((r) =>
-        selection.ressources.includes(r.titre),
-      );
-    });
-
-    return selections;
-  }
-}
+  implements EntrepotSelectionEnseignants {}
