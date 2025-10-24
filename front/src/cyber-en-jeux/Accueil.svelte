961cc3f5db4d8b139d343307cbbbade3b2ece60f
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-10-24 14:01:00 +0200

    [BUG] Applique le style sur un lien
    ...de l’invitation pour rejoindre la communauté CyberEnjeux

--- front/src/cyber-en-jeux/Accueil.svelte
+++ front/src/cyber-en-jeux/Accueil.svelte
@@ -1,5 +1,6 @@
 <script>
   import Citation from '../Citation.svelte';
+  import InvitationARejoindre from './InvitationARejoindre.svelte';
 </script>
 
 <div class="accueil">
@@ -116,15 +117,7 @@
   </dsfr-container>
 
   <dsfr-container>
-    <Citation>
-      Rejoignez la communauté CyberEnJeux pour poser vos questions ou partager
-      votre expérience. <lab-anssi-lien
-        cible="#"
-        titre="Rejoindre la communauté"
-        apparence="lien-texte"
-        target="_blank"
-      ></lab-anssi-lien>
-    </Citation>
+    <InvitationARejoindre />
   </dsfr-container>
 
   <dsfr-container>
---
+++ front/src/cyber-en-jeux/InvitationARejoindre.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Citation from '../Citation.svelte';
+</script>
+
+<Citation>
+  Rejoignez la communauté CyberEnJeux pour poser vos questions ou partager votre
+  expérience. <lab-anssi-lien
+    cible="#"
+    titre="Rejoindre la communauté"
+    apparence="lien-texte"
+    target="_blank"
+  ></lab-anssi-lien>
+</Citation>
+
+<style lang="scss">
+  lab-anssi-lien {
+    color: var(--blue-france-sun-113-625);
+    font-weight: normal;
+  }
+</style>
