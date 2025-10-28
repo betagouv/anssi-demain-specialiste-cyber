b5ec75f820cb7b10f6d2a9e6aa8849c4885d9e1f
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-10-28 16:32:52 +0100

    [SOIN] Extrais le schéma de création d'un jeu

---
+++ back/src/api/schemasJeu.ts
@@ -0,0 +1,78 @@
+import z from 'zod';
+import { disciplines } from '../metier/referentiels/disciplines';
+import { classes } from '../metier/referentiels/classes';
+import { sequences } from '../metier/referentiels/sequence';
+import { categoriesDeJeux } from '../metier/referentiels/categorieDeJeux';
+import { thematiquesDeJeux } from '../metier/referentiels/thematiqueDeJeux';
+
+const chaineNonVide = (message: string) =>
+  z.string(message).trim().min(1, message);
+
+const verifieNoteEvaluation = (message: string) =>
+  z
+    .number()
+    .min(1, {
+      error: message,
+    })
+    .max(5, {
+      error: message,
+    });
+
+export const schemaCreationJeu = z.strictObject({
+  nom: chaineNonVide('Le nom est obligatoire'),
+  nomEtablissement: chaineNonVide('Le nom de l‘établissement est obligatoire'),
+  discipline: z.enum(disciplines, {
+    error: 'La discipline est invalide',
+  }),
+  classe: z.enum(classes, {
+    error: 'La classe est invalide',
+  }),
+  sequence: z.enum(sequences, {
+    error: 'La séquence est invalide',
+  }),
+  eleves: z
+    .array(chaineNonVide('Les prénoms fournis sont invalides'))
+    .nonempty('Au moins un élève est requis'),
+  categorie: z.enum(categoriesDeJeux, {
+    error: 'La catégorie est invalide',
+  }),
+  thematiques: z
+    .array(
+      z.enum(thematiquesDeJeux, {
+        error: 'La thématique est invalide',
+      }),
+    )
+    .nonempty('La thématique est invalide'),
+  description: chaineNonVide('La description du jeu est obligatoire').max(
+    8000,
+    'La description ne peut contenir que 8000 caractères maximum',
+  ),
+  temoignages: z
+    .array(
+      z.strictObject({
+        prenom: chaineNonVide('Le prénom est obligatoire dans un témoignage'),
+        details: chaineNonVide(
+          'Les détails sont obligatoires dans un témoignage',
+        ).max(
+          8000,
+          'Les détails d‘un témoignage ne peuvent excéder 8000 caractères',
+        ),
+      }),
+    )
+    .optional(),
+  evaluationDecouverte: verifieNoteEvaluation(
+    'La note d‘évaluation pour la découverte doit être comprise entre 1 et 5',
+  ),
+  evaluationInteret: verifieNoteEvaluation(
+    'La note d‘évaluation pour l‘intérêt doit être comprise entre 1 et 5',
+  ),
+  evaluationSatisfactionGenerale: verifieNoteEvaluation(
+    'La note d‘évaluation pour la satisfaction générale doit être comprise entre 1 et 5',
+  ),
+  precisions: z
+    .string()
+    .trim()
+    .nonempty('Les précisions ne peuvent pas être vides')
+    .optional(),
+  consentement: z.boolean('Le consentement est invalide').optional(),
+});
