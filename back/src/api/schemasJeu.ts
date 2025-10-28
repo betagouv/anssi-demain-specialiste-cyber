986659c5a262de1c03d670d3de52d77f51897ce8
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-10-28 16:33:54 +0100

    [SOIN] Extrais le schéma de modification d'un jeu

--- back/src/api/schemasJeu.ts
+++ back/src/api/schemasJeu.ts
@@ -76,3 +76,35 @@ export const schemaCreationJeu = z.strictObject({
     .optional(),
   consentement: z.boolean('Le consentement est invalide').optional(),
 });
+
+export const schemaModificationJeu = z.strictObject({
+  nomEtablissement: chaineNonVide(
+    "Le nom de l'établissement est obligatoire",
+  ).optional(),
+  sequence: z.enum(sequences, { error: 'La séquence est invalide' }).optional(),
+  eleves: z.array(z.string()).optional(),
+  discipline: z
+    .enum(disciplines, { error: 'La discipline est invalide' })
+    .optional(),
+  classe: z.enum(classes, { error: 'La classe est invalide' }).optional(),
+  nom: chaineNonVide('Le nom est obligatoire').optional(),
+  categorie: z
+    .enum(categoriesDeJeux, { error: 'La catégorie est invalide' })
+    .optional(),
+  thematiques: z.array(z.enum(thematiquesDeJeux)).optional(),
+  description: chaineNonVide('La description est obligatoire')
+    .max(8000, 'La description ne peut contenir que 8000 caractères maximum')
+    .optional(),
+  consentement: z.boolean().optional(),
+  temoignages: z
+    .array(
+      z.strictObject({
+        prenom: chaineNonVide('Le prénom est obligatoire dans un témoignage'),
+        details: chaineNonVide(
+          'Les détails sont obligatoires dans un témoignage',
+        ).max(8000, 'Les détails ne peuvent excéder 8000 caractères'),
+      }),
+    )
+    .optional(),
+  estCache: z.boolean().optional(),
+});
