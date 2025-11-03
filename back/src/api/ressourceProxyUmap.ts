7814e8fee0fbb845b62db927f952f97879c4c065
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-11-03 15:54:40 +0100

    WIP chargement des CSS et proxy des ressources js

--- back/src/api/ressourceProxyUmap.ts
+++ back/src/api/ressourceProxyUmap.ts
@@ -1,72 +1,43 @@
 import axios from 'axios';
 import { Router } from 'express';
-import { URLSearchParams } from 'url';
 
 export const ressourcesProxyUmap = () => {
   const routeur = Router();
 
-  routeur.use('/umap-proxy', async (requete, reponse) => {
-    const nonce =
-      new URLSearchParams(requete.url).get('nonce')?.replaceAll(' ', '+') || '';
-    const urlUmap = new URL(
-      requete.url.replace(`&nonce=${nonce}`, ''),
-      'https://umap.incubateur.anct.gouv.fr',
+  routeur.use('/umap-proxy', async (_requete, reponse) => {
+    const urlUMap = new URL(
+      'https://umap.incubateur.anct.gouv.fr/fr/map/les-actions-scolaires-de-cybersecurite-2025-2026_3158?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true',
     ).toString();
-    const reponseUmap = await axios.get(urlUmap);
-
-    for (const [key, value] of Object.entries(reponseUmap.headers)) {
-      reponse.setHeader(key, value);
-    }
-
-    if (reponseUmap.status >= 300) {
-      return reponse.sendStatus(reponseUmap.status);
-    }
-
+    const reponseUmap = await axios.get(urlUMap);
     const contenu = reponseUmap.data;
-    // console.log('[DEBUG] contenu: ', urlUmap, contenu);
-
-    const contenuModifiee =
+    const contenuModifie =
       typeof contenu === 'string'
-        ? contenu
-            .replaceAll('/static/', '/umap-proxy/static/')
-            .replaceAll(
-              /<link rel=["']stylesheet["']\s*href=["']([^"']*)["']/g,
-              `<link rel="stylesheet" href="$1?nonce=${nonce}" nonce="${nonce}"`,
-            )
-            .replaceAll(/<script /g, `<script nonce="${nonce}"`)
+        ? contenu.replaceAll(
+            /\/static\/umap(.*?\.css)/g,
+            'https://umap.incubateur.anct.gouv.fr/static/umap$1',
+          )
+        : '';
 
-            .replaceAll(
-              /<script nonce="[^"]*"\s+src=["']([^"']*)["']/g,
-              `<script src="$1?nonce=${nonce}" `,
-            )
-            .replaceAll(
-              /<script nonce="[^"]*"\s+type=["']module["']\s*src=["']([^"']*)["']/g,
-              `<script type="module" src="$1?nonce=${nonce}"`,
-            )
-        : JSON.stringify(contenu);
-
-    if (
-      urlUmap.indexOf(
-        'map/les-actions-scolaires-de-cybersecurite-2025-2026_3158',
-      ) !== -1
-    ) {
-      // eslint-disable-next-line no-console
-      console.log('[DEBUG] Contenu page: ', contenu, contenuModifiee);
-    }
-
-    // console.log('[DEBUG] Nonce copié: ', nonce, urlUmap);
-
-    reponse.setHeader('access-control-allow-origin', '*');
-    reponse.removeHeader('Cross-Origin-Embedder-Policy');
-    reponse.removeHeader('Cross-Origin-Resource-Policy');
     reponse.setHeader(
       'Content-Security-Policy',
-      `script-src 'self' 'nonce-${nonce}'; frame-src 'self';`,
+      `frame-src 'self' https://umap.incubateur.anct.gouv.fr; `,
     );
-    reponse.send(contenuModifiee);
+    reponse.send(contenuModifie);
   });
 
-  routeur.use('/fr/datalayer', () => {});
-
+  routeur.use('/static/umap', async (requete, reponse) => {
+    const urlUMap = new URL(
+      `/static/umap/${requete.path}`,
+      'https://umap.incubateur.anct.gouv.fr',
+    ).toString();
+    try {
+      const reponseUmap = await axios.get(urlUMap);
+      reponse
+        .setHeader('Content-type', 'application/javascript; charset=utf-8')
+        .send(reponseUmap.data);
+    } catch (_err) {
+      /* empty */
+    }
+  });
   return routeur;
 };
