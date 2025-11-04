967c62d8f7a94d7b57deca60e03025d6197b4bda
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-11-04 15:29:55 +0100

    [CEJ] Change le variant du bouton
    ...pour aller sur un fond sombre

--- front/src/cyber-en-jeux/Accueil.svelte
+++ front/src/cyber-en-jeux/Accueil.svelte
@@ -167,7 +167,7 @@
         <div class="action">
           <dsfr-button
             label="Déclarer une action CyberEnJeux"
-            kind="inverted"
+            kind="inverted-secondary"
             target="_blank"
             href="#"
             markup="a"
