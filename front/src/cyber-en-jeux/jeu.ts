5d2bffc7bb5608a7b58b49501969d559635a9797
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-11-07 14:26:10 +0100

    [SOIN][JEU] Extrais les données de construction

--- front/src/cyber-en-jeux/jeu.ts
+++ front/src/cyber-en-jeux/jeu.ts
@@ -13,16 +13,3 @@ export type Jeu = {
   photos: PhotosJeu;
   estCache: boolean;
 };
-
-export type DonneesJeu = {
-  id: string;
-  nom: string;
-  description: string;
-  niveau: string;
-  categorie: string;
-  thematiques: string[];
-  nomEtablissement: string;
-  eleves: string[];
-  reactions: Record<string, number>;
-  photos: PhotosJeu;
-};
