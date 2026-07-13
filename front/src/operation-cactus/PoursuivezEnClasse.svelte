aea5de48b5b7b924b432a52cec166f3b19c5339e
Christophe Cadilhac <christophe@malea.dev>
2026-07-13 12:50:35 +0200

    Mode desktop pour Poursuivez en classe et Témoignagnes

--- front/src/operation-cactus/PoursuivezEnClasse.svelte
+++ front/src/operation-cactus/PoursuivezEnClasse.svelte
@@ -36,9 +36,17 @@
 
 <style lang="scss">
   @use '@style/points-de-rupture' as *;
+  @use '@style/grille' as *;
 
   .poursuivez-en-classe {
     padding: 3.5rem 0 0;
+  }
+
+  @include a-partir-de(lg) {
+    .corps {
+      margin-inline: auto;
+      max-width: taille-pour-colonnes(8);
+    }
   }
 
   .corps {
