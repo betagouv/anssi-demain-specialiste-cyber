2851122e81ca0f0baa9d4eec9328ad52803efb3f
Christophe Cadilhac <christophe@malea.dev>
2026-07-13 12:06:29 +0200

    Ajoute la section "Déclarer une action"

---
+++ front/src/operation-cactus/DeclarerUneAction.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import EncartDeclarerUneAction from '../cyber-en-jeux/EncartDeclarerUneAction.svelte';
+</script>
+
+<div class="declarer-une-action">
+  <dsfr-container>
+    <EncartDeclarerUneAction />
+  </dsfr-container>
+</div>
+
+<style lang="scss">
+  @use '@style/points-de-rupture' as *;
+
+  .declarer-une-action {
+    padding: 3rem 0 3.5rem;
+  }
+</style>
